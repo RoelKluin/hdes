f46303d89f3cbf6bcaf4d9ff64bbbadcb85e4e4e
Roel Kluin <Ar.jayCeekay@gmail.com>
2014-06-20 23:40:49 +0200

    clean it up a bit
    Signed-off-by: Roel Kluin <roel.kluin@gmail.com>

--- main.cpp
+++ main.cpp
@@ -39,79 +39,89 @@ struct uniqct {
         H = kh_init(UQCT);
         is_err = (H == NULL);
     }
+
     void put(const char* key, const char* val)
     {
         if_ever (is_err) return;
         khiter_t k = kh_get(UQCT, H, key);
         if (k == kh_end(H)) {
+
             size_t l = strlen(key) + 1; /* include '\0' */
             is_err = (B + l >= Bend) | (S + 1 >= Send);
-            expected (ok()) {
-                strncpy(B, key, l);
-                k = kh_put(UQCT, H, B, &is_err); //
-                is_err = (is_err < 0);
-                B += l;
-            }
             if_ever (is_err) return;
+
+            strncpy(B, key, l);
+            k = kh_put(UQCT, H, B, &is_err); //
+            is_err = (is_err < 0);
+            if_ever (is_err) return;
+
+            B += l;
             S->l = S->m = 0;
             S->s = NULL;
             kputs(val, S);
             kh_val(H, k) = S++ - Sstart;
+
         } else {
             kputs(val, &Sstart[kh_val(H, k)]);
         }
     }
+
     void dump() {
-        unsigned l;
-        khiter_t *ks;
-        if_ever (is_err) goto err;
+        khiter_t *ks, *kp;
+        if (is_err) goto err;
+
         ks = (khiter_t*)malloc(kh_size(H) * sizeof(khiter_t));
         is_err = (ks == NULL);
-        if_ever (is_err) goto err;
-        l = 0;
+        if (is_err) goto err;
+
+        kp = ks;
         for (khiter_t k = kh_begin(H); k != kh_end(H); ++k)
-            if (kh_exist(H, k)) ks[l++] = k;
+            if (kh_exist(H, k)) *kp++ = k;
 
-        std::sort(ks, ks + l, *this);
+        sort(ks, kp, *this);
 
-        while (l--)
-            cout << kh_key(H, ks[l]) << "\n" << Sstart[kh_val(H, ks[l])].s;
+        while (kp-- != ks)
+            cout << kh_key(H, *kp) << "\n" << Sstart[kh_val(H, *kp)].s;
 
         free(ks);
 err:	while (S-- != Sstart) free(S->s);
-        kh_destroy(UQCT, H);
-
+        if (H) kh_destroy(UQCT, H);
+        H = NULL;
     }
+
     bool operator() (khiter_t a, khiter_t b) /* needed by sort */
     {
         size_t xa, xb;
-        xa = Sstart[kh_val(H, a)].m;
+        xa = Sstart[kh_val(H, a)].m; /* sort by number of entries */
         xb = Sstart[kh_val(H, b)].m;
-        if (xa != xb) return xa > xb; // compares slightly faster since these are powers of 2.
+        if (xa != xb) return xa > xb; /* maxes compare slightly faster since they are powers of 2 */
 
         xa = Sstart[kh_val(H, a)].l;
         xb = Sstart[kh_val(H, b)].l;
         return (xa == xb) ? strcmp(kh_key(H, a), kh_key(H, b)) > 0 : xa > xb;
     }
-    bool ok() { return not is_err; }
+    int is_err;
+
 private:
     KHASH_INIT2(UQCT, kh_inline, const char*, uint32_t, 1, kh_str_hash_func, kh_str_hash_equal)
     char *Bstart, *B, *Bend;
     kstring_t *Sstart, *S, *Send;
     khash_t(UQCT) *H;
-    int is_err;
 };
 
 int main(int argc, const char* argv[])
 {
-    unsigned buflen = 1ul << 28, splen = 1ul << 26; // 67,108,864 reads, 
+    size_t buflen = 1ul << 28, splen = 1ul << 26; // 67,108,864 reads
     char* buf = (char*)malloc(buflen * sizeof(char));
     kstring_t* sp = (kstring_t*)malloc(splen * sizeof(kstring_t));
-    struct uniqct uqct(buf, sp, buflen, splen);
+
     bool order_by_qual = true;
+    // bool discard_comment = true;
+    // bool entrylength_varies_widely = false;
+    struct uniqct uqct(buf, sp, buflen, splen);
 
     string nm, sq, tmp, ql;
-    while (uqct.ok() && getline(cin, nm) && getline(cin, sq) && getline(cin, tmp) && getline(cin, ql)) {
+    while ((not uqct.is_err) && getline(cin, nm) && getline(cin, sq) && getline(cin, tmp) && getline(cin, ql)) {
         tmp = (order_by_qual ? ql + "\t" + nm : nm + "\t" + ql) + "\n";
         uqct.put(sq.c_str(), tmp.c_str());
     }
@@ -119,7 +129,10 @@ int main(int argc, const char* argv[])
     uqct.dump();
     free(buf);
     free(sp);
-    if (uqct.ok()) return EXIT_SUCCESS;
-    cerr << "An error occurred\n";
-    return EXIT_FAILURE;
+
+    if (uqct.is_err) {
+        cerr << "An error occurred\n";
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
 }
