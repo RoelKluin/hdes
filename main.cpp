1d3256f95bb5b5987e66ec65943d46bfd02e56bf
Roel Kluin <Ar.jayCeekay@gmail.com>
2014-06-26 21:50:08 +0200

    improved storage for sorting
    Signed-off-by: Roel Kluin <roel.kluin@gmail.com>

--- main.cpp
+++ main.cpp
@@ -68,50 +68,47 @@ struct uniqct
         char *p, *b = s + l;
 cerr << key << endl << flush;
         khiter_t k = str_kh_get(H, key);
+        size_t i;
         if (k == kh_end(H)) { /* new key */
-            for (unsigned i = 0; i != 4; ++i, ++b) *b = '\0'; /* keycount */
             p = b;
-            for (unsigned i = 0; i != 4; ++i, ++b, l >>= 8)
-                *b = l & 0xff; /* key offset, will be tagged along to last value */
+            for (i = 0; i != 5; ++i, ++b) *b = '\0'; /* keycount */
+            for (i = 0; i != 3; ++i, ++b) *b = '\0'; /* seq length */
 
+            // TODO: form of twobit with N spec
             while ((*b++ = *key) != '\0') ++key;
-            *b++ = '\0';
-cerr << "key at: " << p - s - 4 << endl << flush;
-            k = kh_put(UQCT, H, p - s - 4, &is_err);
+            i = p - s; /* key offset, (denotes last value) */
+            k = kh_put(UQCT, H, i, &is_err);
             is_err = (is_err < 0);
             if_ever (is_err) return;
-cerr << "kh_size: " << kh_size(H) << endl << flush;
-            l = b - s;
+
+cerr << "key at: " << i << endl << "kh_size: " << kh_size(H) << endl << flush;
 
         } else {
-            size_t i = kh_key(H, k);
-cerr << "already observed key at " << i << ":\n";
-            p = s + i;
-cerr << p + 8 << "\n";
+            p = s + kh_key(H, k);
+
+cerr << "already observed key at " << kh_key(H, k) << ":\n" << p + 8 << "\n";
 
             /* increment keycount */
             i = (size_t)++(*p);
             i |= (size_t)(*++p += !i) << 8;
             i |= (size_t)(*++p += !i) << 16;
             keymax |= ((size_t)(*++p += !i) << 24) | i;
-            l = kh_val(H, k);
-            p = s + l + 4;
+            i = kh_val(H, k); /* former value offset */
         }
 
+        p = b;
         // 4 bytes reserved for phred sum
         *b = '\0'; *++b = '\0'; *++b = '\0'; *++b = '\0'; ++b;
-        /* transfer former greatest offset to before last value, put last offset before key */
-        for (unsigned i = 0; i != 4; ++i, ++b, ++p) {
-            *b = *p;
-            *p = l & 0xff;
-cerr << i << ":" << (l & 0xff) << endl;
-            l >>= 8;
+        /* put former value offset here */
+        for (unsigned j = 0; j != 4; ++j, ++b) {
+            *b = i & 0xff;
+cerr << i << ":" << (i & 0xff) << endl;
+            i >>= 8;
         }
-        p = b - 8;
+        // TODO: better qual/name storage
         while ((*b = *val) != '\0') ++b, ++val;
-cerr << p + 8 << endl << flush;
-cerr << "val at: " << p - s << endl << flush;
         kh_val(H, k) = p - s;
+cerr << p + 8 << endl << "val at: " << p - s << endl << flush;
         l = ++b - s;
     }
 
@@ -119,11 +116,12 @@ cerr << "val at: " << p - s << endl << flush;
         if (not is_err) {
             khiter_t *ks, *kp;
             size_t *vp, *vs;
-            s[l] = '\0';
+            s[l] = '\0'; // end last value
 
             kp = ks = (khiter_t*)malloc(kh_size(H) * sizeof(khiter_t));
             for (khiter_t k = kh_begin(H); k != kh_end(H); ++k)
                 if (kh_exist(H, k)) { *kp = k; ++kp; }
+            ++keymax;
             kroundup32(keymax);
             cerr << "== Max number of identical sequences near " << keymax << endl;
 
@@ -132,8 +130,8 @@ cerr << "val at: " << p - s << endl << flush;
             sort(ks, kp, *this);
 
             //at = (uint64_t)((s[at+4] << 32)|(s[at+3] << 24)|(s[at+2] << 16)|(s[at+1] << 8)|s[at]);
-            while (kp-- != ks) { /* loop over keys */
-                size_t end = kh_key(H, *kp);
+            while (kp != ks) { /* loop over keys */
+                size_t end = kh_key(H, *--kp);
                 size_t i = kh_val(H, *kp);
                 cout << s + end + 8 << ":key\n";
                 cout << s + i + 8 << ":val\n";
@@ -141,28 +139,29 @@ cerr << "val at: " << p - s << endl << flush;
                 vp = vs;
                 do { /* loop over values belonging to key */
 cerr << "offsetted key/val at: " << i + 8 << endl;
+                    *vp = i;
+                    ++vp;
                     char* b = s + i + 8;
                     i = decr4chtou32(b, i);
 
 cerr << "got key/val: " << i << "/" << end << endl;
+
 if (i > (1 << 12)) { cerr << "ERROR\n"; break; }
 if (vp - vs != 0 && vp[-1] == i) { cerr << "ERROR2\n"; break; }
 if ((vp - vs > 1) && vp[-2] == i) { cerr << "ERROR3\n"; break; }
+if (vp - vs > keymax) { cerr << "ERROR4\n"; break; }
 
-                    *vp = i;
-                    ++vp;
                 } while (i != end);
 
 if (i > (1 << 12)) break;
-if (vp - vs != 1 && vp[-2] == i) break;
-if ((vp - vs > 2) && vp[-3] == i) { cerr << "ERROR4\n"; break; }
+//if (vp - vs != 1 && vp[-2] == i) break;
+//if ((vp - vs > 2) && vp[-3] == i) { cerr << "ERROR5\n"; break; }
+//if (vp - vs >= keymax) { cerr << "ERROR4\n"; break; }
 
-cerr << "sort...\n";
-                if (--vp != vs)
-                    sort(vp, vs, *this);
+cerr << "sorting " << vp - vs << " keys ...\n";
+                sort(vs, vp, *this);
 
-                while (vp != vs) { cout << *vp << "\n"; --vp; }
-                cout << *vp << "\n";
+                while (vp != vs) cout << s + *--vp + 8 << "\n";
             }
             free(vs);
             free(ks);
