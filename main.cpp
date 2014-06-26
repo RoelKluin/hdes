9e6a7816586f16bd26be1d13356087bc78227d6d
Roel Kluin <Ar.jayCeekay@gmail.com>
2014-06-26 20:01:24 +0200

    first working version for fastq sorting
    Signed-off-by: Roel Kluin <roel.kluin@gmail.com>

--- main.cpp
+++ main.cpp
@@ -18,117 +18,211 @@
 // make && zgrep -E -o "^[ACTGN]{6}" /home/roel/dev/rproject/rmap/hg19.fa.gz | head -n 100000 | valgrind ./uqct
 
 #include <stdint.h>
+#include <assert.h>
+
 #include <iostream>
 #include <algorithm>
 
 #include "khash.h"
 #include "kstring.h"
 
+#define MAX_NAME_ETC    1024
+
 #define likely(x)       __builtin_expect((x),1)
 #define unlikely(x)     __builtin_expect((x),0)
 #define if_ever(err)    if (unlikely(err))
-#define expected(T)     if (likely(T))
+#define expect(T)       if (likely(T))
+
+static unsigned phredtoe10[] = { 1, 1, 1, 1, 2, 3, 3, 5, 6, 7, 10,
+    12, 15, 19, 25, 31, 39, 50, 63, 79, 100,
+    125, 158, 199, 251, 316, 398, 501, 630, 794, 1000,
+    1258, 1584, 1995, 2511, 3162, 3981, 5011, 6309, 7943, 10000 };
 
 using namespace std;
 
-struct uniqct {
-    uniqct(char* b, kstring_t* s, size_t l, size_t n) :
-        Bstart(b), B(b), Bend(b + l),
-        Sstart(s), S(s), Send(s + n)
+#define decr4chtou32(b, i) ({   \
+    i = (uint8_t)*--b; i <<= 8;          \
+    i |= (uint8_t)*--b; i <<= 8;         \
+    i |= (uint8_t)*--b; i <<= 8;         \
+    i | (uint8_t)*--b;                   \
+})
+
+struct uniqct
+{
+    uniqct(size_t rl) : is_err(0), l(0),
+        m(1ul << 23), fq_ent_max((rl << 1) + MAX_NAME_ETC), keymax(1)
     {
         H = kh_init(UQCT);
-        is_err = (H == NULL);
+        s = (char*)malloc(m);
+        *s = '\0';
+        kroundup32(fq_ent_max);
     }
 
     void put(const char* key, const char* val)
     {
         if_ever (is_err) return;
-        khiter_t k = kh_get(UQCT, H, key);
-        if (k == kh_end(H)) {
+        if (l + fq_ent_max >= m) {
+            m <<= 1;
+            s = (char*)realloc(s, m);
+        }
+        char *p, *b = s + l;
+cerr << key << endl << flush;
+        khiter_t k = str_kh_get(H, key);
+        if (k == kh_end(H)) { /* new key */
+            for (unsigned i = 0; i != 4; ++i, ++b) *b = '\0'; /* keycount */
+            p = b;
+            for (unsigned i = 0; i != 4; ++i, ++b, l >>= 8)
+                *b = l & 0xff; /* key offset, will be tagged along to last value */
 
-            size_t l = strlen(key) + 1; /* include '\0' */
-            is_err = (B + l >= Bend) | (S + 1 >= Send);
-            if_ever (is_err) return;
-
-            strncpy(B, key, l);
-            k = kh_put(UQCT, H, B, &is_err); //
+            while ((*b++ = *key) != '\0') ++key;
+            *b++ = '\0';
+cerr << "key at: " << p - s - 4 << endl << flush;
+            k = kh_put(UQCT, H, p - s - 4, &is_err);
             is_err = (is_err < 0);
             if_ever (is_err) return;
-
-            B += l;
-            S->l = S->m = 0;
-            S->s = NULL;
-            kputs(val, S);
-            kh_val(H, k) = S++ - Sstart;
+cerr << "kh_size: " << kh_size(H) << endl << flush;
+            l = b - s;
 
         } else {
-            kputs(val, &Sstart[kh_val(H, k)]);
+            size_t i = kh_key(H, k);
+cerr << "already observed key at " << i << ":\n";
+            p = s + i;
+cerr << p + 8 << "\n";
+
+            /* increment keycount */
+            i = (size_t)++(*p);
+            i |= (size_t)(*++p += !i) << 8;
+            i |= (size_t)(*++p += !i) << 16;
+            keymax |= ((size_t)(*++p += !i) << 24) | i;
+            l = kh_val(H, k);
+            p = s + l + 4;
         }
+
+        // 4 bytes reserved for phred sum
+        *b = '\0'; *++b = '\0'; *++b = '\0'; *++b = '\0'; ++b;
+        /* transfer former greatest offset to before last value, put last offset before key */
+        for (unsigned i = 0; i != 4; ++i, ++b, ++p) {
+            *b = *p;
+            *p = l & 0xff;
+cerr << i << ":" << (l & 0xff) << endl;
+            l >>= 8;
+        }
+        p = b - 8;
+        while ((*b = *val) != '\0') ++b, ++val;
+cerr << p + 8 << endl << flush;
+cerr << "val at: " << p - s << endl << flush;
+        kh_val(H, k) = p - s;
+        l = ++b - s;
     }
 
-    void dump() {
-        khiter_t *ks, *kp;
-        if (is_err) goto err;
+    void dump() { /* must always be called to clean up */
+        if (not is_err) {
+            khiter_t *ks, *kp;
+            size_t *vp, *vs;
+            s[l] = '\0';
 
-        ks = (khiter_t*)malloc(kh_size(H) * sizeof(khiter_t));
-        is_err = (ks == NULL);
-        if (is_err) goto err;
+            kp = ks = (khiter_t*)malloc(kh_size(H) * sizeof(khiter_t));
+            for (khiter_t k = kh_begin(H); k != kh_end(H); ++k)
+                if (kh_exist(H, k)) { *kp = k; ++kp; }
+            kroundup32(keymax);
+            cerr << "== Max number of identical sequences near " << keymax << endl;
 
-        kp = ks;
-        for (khiter_t k = kh_begin(H); k != kh_end(H); ++k)
-            if (kh_exist(H, k)) *kp++ = k;
+            vs = (size_t*)malloc(keymax * sizeof(size_t));
 
-        sort(ks, kp, *this);
+            sort(ks, kp, *this);
 
-        while (kp-- != ks)
-            cout << kh_key(H, *kp) << "\n" << Sstart[kh_val(H, *kp)].s;
+            //at = (uint64_t)((s[at+4] << 32)|(s[at+3] << 24)|(s[at+2] << 16)|(s[at+1] << 8)|s[at]);
+            while (kp-- != ks) { /* loop over keys */
+                size_t end = kh_key(H, *kp);
+                size_t i = kh_val(H, *kp);
+                cout << s + end + 8 << ":key\n";
+                cout << s + i + 8 << ":val\n";
 
-        free(ks);
-err:	while (S-- != Sstart) free(S->s);
-        if (H) kh_destroy(UQCT, H);
-        H = NULL;
+                vp = vs;
+                do { /* loop over values belonging to key */
+cerr << "offsetted key/val at: " << i + 8 << endl;
+                    char* b = s + i + 8;
+                    i = decr4chtou32(b, i);
+
+cerr << "got key/val: " << i << "/" << end << endl;
+if (i > (1 << 12)) { cerr << "ERROR\n"; break; }
+if (vp - vs != 0 && vp[-1] == i) { cerr << "ERROR2\n"; break; }
+if ((vp - vs > 1) && vp[-2] == i) { cerr << "ERROR3\n"; break; }
+
+                    *vp = i;
+                    ++vp;
+                } while (i != end);
+
+if (i > (1 << 12)) break;
+if (vp - vs != 1 && vp[-2] == i) break;
+if ((vp - vs > 2) && vp[-3] == i) { cerr << "ERROR4\n"; break; }
+
+cerr << "sort...\n";
+                if (--vp != vs)
+                    sort(vp, vs, *this);
+
+                while (vp != vs) { cout << *vp << "\n"; --vp; }
+                cout << *vp << "\n";
+            }
+            free(vs);
+            free(ks);
+        }
+	free(s);
+        kh_destroy(UQCT, H);
     }
-
-    bool operator() (khiter_t a, khiter_t b) /* needed by sort */
+    inline bool operator() (khiter_t a, khiter_t b) /* needed by sort */
     {
-        size_t xa, xb;
-        xa = Sstart[kh_val(H, a)].m; /* sort by number of entries */
-        xb = Sstart[kh_val(H, b)].m;
-        if (xa != xb) return xa > xb; /* maxes compare slightly faster since they are powers of 2 */
+        char *sa = s + kh_key(H, a) + 8;
+        char *sb = s + kh_key(H, b) + 8;
+        while (*sa == *sb) { ++sa; ++sb; }
+        return *sa > *sb;
+    }
 
-        xa = Sstart[kh_val(H, a)].l;
-        xb = Sstart[kh_val(H, b)].l;
-        return (xa == xb) ? strcmp(kh_key(H, a), kh_key(H, b)) > 0 : xa > xb;
+    inline bool operator() (size_t a, size_t b) /* needed by sort */
+    {
+        return strcmp(s + a, s + b);
     }
     int is_err;
 
 private:
-    KHASH_MAP_INIT_STR(UQCT, uint32_t)
-    char *Bstart, *B, *Bend;
-    kstring_t *Sstart, *S, *Send;
+#define my_str_hash_equal(a, b) (strcmp(s + a + 8, s + b + 8) == 0)
+#define my_str_hash_func(key) __ac_X31_hash_string(s + key + 8)
+    KHASH_INIT2(UQCT, kh_inline, uint32_t, uint32_t, 1, my_str_hash_func, my_str_hash_equal)
+    kh_inline khint_t str_kh_get(const kh_UQCT_t *h, const char* key)
+    {
+        if (h->n_buckets) {
+            khint_t inc, k, i, last, mask;
+            mask = h->n_buckets - 1;
+            k = __ac_X31_hash_string(key); i = k & mask;
+            inc = __ac_inc(k, mask); last = i; // inc==1 for linear probing
+            while (!__ac_isempty(h->flags, i) && (__ac_isdel(h->flags, i) || strcmp(s + h->keys[i] + 8, key) != 0)) {
+                i = (i + inc) & mask;
+                if (i == last) return h->n_buckets;
+            }
+            return __ac_iseither(h->flags, i)? h->n_buckets : i;
+        } else return 0;
+    }
+//KHASH_MAP_INIT_STR(UQCT, uint32_t)
     khash_t(UQCT) *H;
+    size_t l, m, fq_ent_max, keymax;
+    char *s;
 };
+
 
 int main(int argc, const char* argv[])
 {
-    size_t buflen = 1ul << 28, splen = 1ul << 26; // 67,108,864 reads
-    char* buf = (char*)malloc(buflen * sizeof(char));
-    kstring_t* sp = (kstring_t*)malloc(splen * sizeof(kstring_t));
-
     bool order_by_qual = true;
     // bool discard_comment = true;
     // bool entrylength_varies_widely = false;
-    struct uniqct uqct(buf, sp, buflen, splen);
+    struct uniqct uqct(51);
 
     string nm, sq, tmp, ql;
     while ((not uqct.is_err) && getline(cin, nm) && getline(cin, sq) && getline(cin, tmp) && getline(cin, ql)) {
-        tmp = (order_by_qual ? ql + "\t" + nm : nm + "\t" + ql) + "\n";
+        tmp = order_by_qual ? ql + "\t" + nm : nm + "\t" + ql;
         uqct.put(sq.c_str(), tmp.c_str());
     }
 
     uqct.dump();
-    free(buf);
-    free(sp);
 
     if (uqct.is_err) {
         cerr << "An error occurred\n";
--- test.sh
+++ test.sh
@@ -1,6 +1,9 @@
 
-make && zgrep -E -o "^[ACTGN]{6}" /home/roel/dev/rproject/rmap/hg19.fa.gz | head -n 800000 | valgrind ./uqct 2>&1 | less
+#make && zgrep -E -o "^[ACTGN]{6}" /home/roel/dev/rproject/rmap/hg19.fa.gz | head -n 800000 | valgrind ./uqct 2>&1 | less
 
-time zgrep -E -o "^[ACTGN]{6}" /home/roel/dev/rproject/rmap/hg19.fa.gz | head -n 800000 | ./uqct
+#time zgrep -E -o "^[ACTGN]{6}" /home/roel/dev/rproject/rmap/hg19.fa.gz | head -n 800000 | ./uqct
 
 
+make && zcat /home/roel/dev/git/bwa/my/bwa/test/ce_med.fq.gz | valgrind ./uqct 1>/dev/null 2> valg.err
+
+make && zcat /home/roel/dev/git/bwa/my/bwa/test/ce_med.fq.gz | ./uqct | less
