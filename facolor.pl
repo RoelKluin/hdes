e9cdd90174dddc6c2ad57a2853b17367f4bd20b5
Roel Kluin <Ar.jayCeekay@gmail.com>
2015-04-01 22:54:58 +0200

    improve searching in nacolor.pl with extend
    Signed-off-by: Roel Kluin <roel.kluin@gmail.com>

--- facolor.pl
+++ facolor.pl
@@ -7,11 +7,11 @@ use strict;
 
 my (%h, %g);
 my @seq = qw/T A C G U N/;
-
 my ($help, $man, $re);
+my $extend = 0;
 
 Getopt::Long::Configure("no_ignore_case", "prefix_pattern=(--|-)");
-GetOptions('help|?|h' => \$help, man => \$man, 're=s' => \$re);
+GetOptions('help|?|h' => \$help, man => \$man, 're=s' => \$re, 'extend|e=i' => \$extend);
 
 pod2usage(-verbose => 2) if defined $man;
 pod2usage(0) if defined $help;
@@ -19,16 +19,28 @@ $re = defined($re) ? ($re =~ /^\(/ ? qr/$re/ : qr/($re)/) : qr//;
 
 $h{$seq[$_]} = "\e[".(31+$_).";1m".$seq[$_] for 0..$#seq;
 $g{$seq[$_]} = "\e[".(91+$_).";1m".$seq[$_] for 0..$#seq;
-
-while (<>) {
+$_ = <>;
+s/[\n\r]*$//;
+my $givemeabreak;
+while ($_) {
         s/^RepBase.*\0+[^>]+>/>/; #repbase meuk
-	s/[\n\r]*$//;
-        print "\e[1m$_\e[0m\n" and next if /^(?:==|\s*$|>)/;
+        $givemeabreak = $_ =~ /^(?:==|\s*$|>)/;
+        print "\e[1m$_\e[0m" and next if $givemeabreak;
 	print join("", map { $_ =~ $re ? "\e[7m".join('',
                     map { $g{uc$_} // $_ } split //)."\e[0m" :
                 "\e[1m".join('', map { $h{uc$_} // $_ } split //)."\e[0m"
-                } split $re)."\e[0m\n";
+                } split $re)."\e[0m";
+} continue {
+    $_ = <>;
+    print "\n" and last if not defined $_;
+    s/[\n\r]*$//;
+    if ($givemeabreak || /^(?:==|\s*$|>)/) {
+        print "\n";
+    } else {
+        print substr($_, 0, $extend)."\n";
+    }
 }
+
 __END__
 
 =head1 NAME
