87a94b2c7a80ff8be5c9212e66f837c78fa92c15
Roel Kluin <Ar.jayCeekay@gmail.com>
2016-06-14 22:22:37 +0200

    simplify mantra printing
    Signed-off-by: Roel Kluin <roel.kluin@gmail.com>

--- mantra.cpp
+++ mantra.cpp
@@ -23,22 +23,13 @@ show_mantras(kct_t C*C kc, std::list<Mantra>::iterator here)
     if (it != kc->bnd->end()) {
         do {
             uint32_t ke = (*it).ke;
-            if (ke <= kc->hkoffs[kc->h_l-1]) {
-                while (ke > kc->hkoffs[h - kc->h])
-                    ++h;
-            } else {
-                h = kc->h;
-                for (uint32_t* hkoffs = kc->hkoffs + kc->h_l;
-                        hkoffs != (kc->hkoffs + kc->hkoffs_l) && ke >= *hkoffs; ++hkoffs, ++h)
-                    if (h == kc->h + kc->h_l)
-                        h = kc->h;
-            }
+            while (h - kc->h != (*it).ho)
+                ++h;
             //EPR("%s",  ke != kc->hkoffs[h - kc->h] ? "kepos(kc, it) - 2" : "h->end");
             uint32_t end = ke != kc->hkoffs[h - kc->h] ? kepos(kc, it) - 2 : h->end;
 
             EPR("[%u%c]:\t>%s (%u+)%u - %u\t(ke:%u)", j++, it==here?'*':' ',
                     kc->id + h->ido, (*it).corr, (*it).s >>1, end >> 1, ke);
-            ++h;
         } while (++it != kc->bnd->end());
     } else {
         EPR("[ entirely mapable ]");
